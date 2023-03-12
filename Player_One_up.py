df2823e2b03ca6e46bbfbe2ea56661a4c71ebbea
velicki <126986884+velicki@users.noreply.github.com>
2023-03-12 01:44:04 +0100

    Revert "Add functions (#1)"
    This reverts commit 8ff6cb128c7002bbc73ba0bc8b79468c1869ef70.

--- Player_One_up.py
+++ Player_One_up.py
@@ -2,11 +2,9 @@ from tkinter import *
 from tkinter.font import BOLD
 import game_controll
 
-#-----------------------------------------------
 
-def Map_Button(jezik,p1color,p2color,p1_up,p1_down,p2_up,p2_down):     # Novi prozor kada se klikne dugme srpski ili engleski jezik
 
-#-----------------------------------------------
+def Map_Button(jezik,p1color,p2color,p1_up,p1_down,p2_up,p2_down):                         # Novi prozor kada se klikne dugme srpski ili engleski jezik
 
     def start_new_game(event):
         if (event.keysym != p1_down and event.keysym != p2_up and event.keysym != p2_down):
@@ -15,56 +13,49 @@ def Map_Button(jezik,p1color,p2color,p1_up,p1_down,p2_up,p2_down):     # Novi pr
         menu_controll.destroy()
         game_controll.Menu_controll(jezik,p1color,p2color,p1_up,p1_down,p2_up,p2_down)
 
-    def chosen_language(jezik):
-        if jezik == "serbian":
-            main_text = "Mico klikni dugme za gore!"
-            copyright_text = "Igricu napravio Velicki"
 
-        elif jezik == "english":
-            main_text = "Mica press button for up"
-            copyright_text = "copyright by Velicki"
+    menu_controll = Tk ()
 
-        return (main_text,copyright_text)
+    pokret = p1_up
 
-    def windows_configuration():
-        menu_controll.geometry("600x600")                  # Velicina prozora
-        menu_controll.title(chosen_language(jezik)[0])     # Ime prozora
-        menu_controll.resizable(False, False)
-        icon = PhotoImage(file="Icon.png")                 # Slika je prebacena u format koji moze da cita python
-        menu_controll.iconphoto(True, icon)                # Ikonica prozora
-        menu_controll.config(bg="black")                   # Boja pozadine prozora
+    if jezik == "serbian":
+        main_text = "Mico klikni dugme za gore!"
+        copyright_text = "Igricu napravio Velicki"
 
-    def naslov():
-        naslov = Label(menu_controll, 
-                    text=chosen_language(jezik)[0], 
+    elif jezik == "english":
+        main_text = "Mica press button for up"
+        copyright_text = "copyright by Velicki"
+
+    menu_controll.geometry("600x600")                  # Velicina prozora
+    menu_controll.title("change_game_controll")                # Ime prozora
+    menu_controll.resizable(False, False)
+
+    icon = PhotoImage(file="Icon.png")          # Slika je prebacena u format koji moze da cita python
+    menu_controll.iconphoto(True, icon)                # Ikonica prozora
+
+    menu_controll.config(bg="black")           # Boja pozadine prozora
+
+    menu_controll.bind("<Key>",start_new_game)   #ova komanda prihvata karakter sa tastature
+
+    naslov = Label(menu_controll, 
+                    text=main_text, 
                     font=("Arial", 20, BOLD), 
                     fg="red", 
                     bg="black",
                     activeforeground="red",
                     activebackground="black",  
                     relief=RAISED, 
-                    bd=10)                    #Napravljen i dekorisan naslov
-        return naslov
+                    bd=10 
+                    #pady=10, 
+                    #padx=10, 
+                    )             #Napravljen i dekorisan naslov
 
-    def copyright():
-        copyright = Label(menu_controll, 
-                    text=chosen_language(jezik)[1], 
+    copyright = Label(menu_controll, 
+                    text=copyright_text, 
                     font=("Arial", 10), 
                     fg="red", 
                     bg="black")                # Informacija na dnu stranice ko je napravio igricu
-        return copyright
 
-#-----------------------------------------------
+    naslov.pack(pady=150)                       # Dugme je ubaceno u prozor
 
-    menu_controll = Tk ()
-
-    pokret = p1_up
-
-    windows_configuration()
-
-    menu_controll.bind("<Key>",start_new_game)   #ova komanda prihvata karakter sa tastature
-
-#-----------------------------------------------
-
-    naslov().pack(pady=150)                       # Dugme je ubaceno u prozor
-    copyright().pack(side="bottom")               # Informacija na dnu je ubacena u prozor
+    copyright.pack(side="bottom")              # Informacija na dnu je ubacena u prozor
--- Tennis.py
+++ Tennis.py
@@ -2,8 +2,6 @@ from tkinter import *
 from tkinter.font import BOLD
 import Menu_of_game
 
-#-----------------------------------------------
-
 class buttonb(object):
     def __init__(self, d_text, d_Serbian_Flag, d_command):
         self.d_Serbian_Flag = d_Serbian_Flag
@@ -23,67 +21,63 @@ class buttonb(object):
                 #pady=10, 
                 #padx=10, 
                 image= self.d_Serbian_Flag, 
-                compound="left",
-                command= self.d_command)
+                compound="left", command= self.d_command)
         return (s_f)
 
-#-----------------------------------------------
-
-def windows_configuration():
-    window.geometry("600x600")                              # Velicina prozora
-    window.title("Mica VS Bica")                            # Ime prozora
-    window.resizable(False, False)
-    window.iconphoto(True, PhotoImage(file="Icon.png"))     # Ikonica prozora
-    window.config(bg="black")                               # Boja pozadine prozora
-
-def setup_defoult_controlls():
-    p2_up = "Up"
-    p2_down = "Down"
-    p1_up = "w"
-    p1_down = "s"
-    p1color = "red"
-    p2color = "blue"
-    return (p1_up,p1_down,p2_up,p2_down,p1color,p2color)
 
 def clicked_serbian():
     jezik = "serbian"
-    x = setup_defoult_controlls()
+    p1color = "red"
+    p2color = "blue"
     window.destroy()
-    Menu_of_game.menu_of_game(jezik,x[4],x[5],x[0],x[1],x[2],x[3])
+    Menu_of_game.menu_of_game(jezik,p1color,p2color,p1_up,p1_down,p2_up,p2_down)
     
 
 def clicked_english():
     jezik = "english"
-    x = setup_defoult_controlls()
+    p1color = "red"
+    p2color = "blue"
     window.destroy()
-    Menu_of_game.menu_of_game(jezik,x[4],x[5],x[0],x[1],x[2],x[3])
+    Menu_of_game.menu_of_game(jezik,p1color,p2color,p1_up,p1_down,p2_up,p2_down)
+    
 
-def copyright():
-    copyright = Label(window, 
-                text="copyright by Velicki", 
-                font=("Arial", 10), 
-                fg="red", 
-                bg="black")                     # Informacija na dnu stranice ko je napravio igricu
-    return copyright
-
-#-----------------------------------------------
 
 window = Tk ()                                  # inicializiran prozor
 
-windows_configuration()
+p2_up = "Up"
+p2_down = "Down"
+p1_up = "w"
+p1_down = "s"
 
-#-----------------------------------------------
+window.geometry("600x600")                  # Velicina prozora
+window.title("Mica VS Bica")                # Ime prozora
+window.resizable(False, False)
 
-s_f_b = buttonb("SRPSKI", PhotoImage(file="SerbianFlag.png"), clicked_serbian)   # Napravljeno i dekorisano je dugme srpski jezikj
 
-e_f_b = buttonb("ENGLISH", PhotoImage(file="EnglishFlag.png"), clicked_english)  # Napravljeno i dekorisano je dugme engleski jezik
+icon = PhotoImage(file="Icon.png")          # Slika je prebacena u format koji moze da cita python
+window.iconphoto(True, icon)                # Ikonica prozora
 
-#-----------------------------------------------
+window.config(bg="black")                   # Boja pozadine prozora
 
-s_f_b.create_button().pack(pady=50)             # Dugme za Srpski je ubaceno u prozor
-e_f_b.create_button().pack(pady=50)             # Dugme za Engleski je ubaceno u prozor
-copyright().pack(side="bottom")                 # Informacija na dnu je ubacena u prozor
+Serbian_Flag = PhotoImage(file="SerbianFlag.png")
+English_Flag = PhotoImage(file="EnglishFlag.png")
 
-#-----------------------------------------------
+
+s_f_b = buttonb("SRPSKI", Serbian_Flag, clicked_serbian)
+serbian_flag = s_f_b.create_button()         # Napravljeno i dekorisano je dugme srpski jezikj
+
+e_f_b = buttonb("ENGLISH", English_Flag, clicked_english)
+english_flag = e_f_b.create_button()         # Napravljeno i dekorisano je dugme engleski jezik
+
+copyright = Label(window, 
+                text="copyright by Velicki", 
+                font=("Arial", 10), 
+                fg="red", 
+                bg="black")                # Informacija na dnu stranice ko je napravio igricu
+
+
+serbian_flag.pack(pady=50)                       # Dugme za Srpski je ubaceno u prozor
+english_flag.pack(pady=50)                       # Dugme za Engleski je ubaceno u prozor
+copyright.pack(side="bottom")              # Informacija na dnu je ubacena u prozor
 
 window.mainloop()                               # kraj inicializiranog prozora
